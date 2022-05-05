2199aeacfddff0070ff8291f6e827ee494a8ada5
nvinayvarma189 <vinay.n.varma189@gmail.com>
2022-05-06 02:46:57 +0530

    add comment log statements and delete log statements

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
-export const DIVIDER_TEXT = "===============================================================";
-export const LOG_STATEMENT = "print";
-export const DIVIDER_LOG_STATEMENT = `${LOG_STATEMENT}("${DIVIDER_TEXT}")`;
+export const DIVIDER_TEXT: string = "===============================================================";
+export const LOG_STATEMENT: string = "print";
+export const REGEX_LOG_STATEMENT: { [key: string]: RegExp } = {"print": /\bprint\s*\(/, "logger": /\blogger.(debug|error|success|warning|log|info)\s*\(/};
+export const DIVIDER_LOG_STATEMENT: string = `${LOG_STATEMENT}("${DIVIDER_TEXT}")`;
