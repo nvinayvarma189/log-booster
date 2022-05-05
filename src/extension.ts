4dafafa373133792e0ad3434a31e9fb0a3429e81
nvinayvarma189 <vinay.n.varma189@gmail.com>
2022-05-05 16:50:32 +0530

    add feature to insert before current line

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,14 @@ import * as consts from './consts';
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	let disposable = vscode.commands.registerCommand('log-booster.insertStatements', async () => {
+	let disposable = vscode.commands.registerCommand('log-booster.insertStatements', async (args) => {
 		const editor = vscode.window.activeTextEditor;
 		
 		if (!editor) {
 			vscode.window.showInformationMessage('No active editor. Please open a Python file.');
 			return; // No open text editor
 		}
-
+		const insertBefore = args["insertBefore"];
 		const selection = editor.selection;
 		const selectedText = editor.document.getText(selection);
 		const fileName = editor.document.fileName;
@@ -27,7 +27,14 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		await vscode.commands.executeCommand('editor.action.insertLineAfter');
+		if (insertBefore){
+			await vscode.commands.executeCommand('editor.action.insertLineBefore');
+		}
+		else{
+			await vscode.commands.executeCommand('editor.action.insertLineAfter');
+		}
+
+		// vscode.window.activeTextEditor?.options.tabSize will give us tab size. TODO: check if this is the best way to get tab size
 		let curPos = editor.selection.active;
 		const indent = editor.document.lineAt(curPos.line).text; // number of spaces at the beginning of the line
 		const logStatementText = await getLogStatement(selectedText);
